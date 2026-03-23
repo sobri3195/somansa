d942f330ed245d90c99b21e1b2b924731f945279
Muhammad Sobri Maulana <sobri3195@gmail.com>
2026-03-23 08:57:19 +0700

    Fix production landing page delivery

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import React from 'https://esm.sh/react@18.3.1';
 import { createRoot } from 'https://esm.sh/react-dom@18.3.1/client';
-import { HelmetProvider } from 'https://esm.sh/react-helmet-async@2.0.5';
 import { App } from './App.js';
+import './styles.css';
 
-createRoot(document.getElementById('root')).render(
-  React.createElement(HelmetProvider, null, React.createElement(App)),
-);
+createRoot(document.getElementById('root')).render(React.createElement(React.StrictMode, null, React.createElement(App)));
